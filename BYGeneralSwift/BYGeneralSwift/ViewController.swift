8f3f12f0936bc8569a38ea74d5d81070ff2c9665
boye.hou <boye.hou@advance.ai>
2021-12-07 11:40:53 +0800

    init project
    init project

---
+++ BYGeneralSwift/BYGeneralSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BYGeneralSwift
+//
+//  Created by boye on 2021/12/7.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
